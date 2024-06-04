ee735ce5d31d3bdf1233abbf3e9383c740720b0c
Sebastian Rojas <sebasleprodevj12@gmail.com>
2024-06-04 18:23:39 -0400

    changes of changes  in db

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,33 @@
 require("dotenv").config();
 const port = process.env.PORT ?? 3000;
 const app = require("./src/App");
-const { initialzeDB } = require("./database");
+const createTable = require("./database");
+const { Client } = require("pg");
+
+const client = new Client({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+});
+
+client
+  .connect()
+  .then(() => {
+    console.log("Connected to PostgreSQL");
+    return client.query(createTable);
+  })
+  .then(() => {
+    console.log("Table created succesfully");
+  })
+  .catch((err) => {
+    console.error("Error executing query", err.stack);
+  })
+  .finally(() => {
+    client.end();
+  });
 
 app.listen(port, async () => {
   console.log(`app running at http://localhost:${port}`);
-  await initialzeDB();
 });
