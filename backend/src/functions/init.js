f49918df46239d7460bfa86a3113b3364271bff9
Sebastian Rojas <sebasleprodevj12@gmail.com>
2024-06-04 18:41:45 -0400

    db.sql  added

--- backend/src/functions/init.js
+++ backend/src/functions/init.js
@@ -1,13 +1,13 @@
-// require("dotenv").config();
-// const fs = require("fs");
-// const db = require('better-sqlite3')(process.env.DB_SOURCE, {});
+require("dotenv").config();
+const fs = require("fs");
+const db = require("better-sqlite3")(process.env.DB_SOURCE, {});
 
-// async function init() {
-//     const script = fs.readFileSync("./database/db.sql", "utf-8");
-//     await db.exec(script);
-// }
+async function init() {
+  const script = fs.readFileSync("./database/db.sql", "utf-8");
+  await db.exec(script);
+}
 
-// module.exports = {
-//   db,
-//   init,
-// };
+module.exports = {
+  db,
+  init,
+};
