d565b0375ab3206c563d22034ee13052a05c4aef
Sebastian Rojas <sebasleprodevj12@gmail.com>
2024-06-04 17:24:15 -0400

    database added

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const port = process.env.PORT ?? 3000;
 const app = require("./src/App");
-const { initialzeDB } = require("./src/functions/init");
+const { initialzeDB } = require("./database");
 
 app.listen(port, async () => {
   console.log(`app running at http://localhost:${port}`);
--- backend/src/functions/init.js
+++ backend/src/functions/init.js
@@ -1,30 +1,4 @@
 const createTable = require("../../database/db");
-const { Client } = require("pg");
-
-const client = new Client({
-  connectionString:
-    "postgres://sebastian_12:TUyquMzARc7ly23aRP1LmR8DNaRwxAnI@dpg-cpf4anrtg9os73b7bfng-a/main_9tbv",
-  ssl: { rejectUnauthorized: false },
-});
-
-async function initialzeDB() {
-  try {
-    await client.connect();
-    console.log("Connected to the database");
-    await client.query(createTable);
-    console.log("Tables created successfully");
-  } catch (err) {
-    console.error("Error connecting to the database or creating tables:", err);
-  } finally {
-    await client.end();
-    console.log("Database connection closed");
-  }
-}
-
-initialzeDB();
-module.exports = {
-  initialzeDB,
-};
 
 // require("dotenv").config();
 // const fs = require("fs");
